3a28fa2464caa8ef97cb29a15dcec1e5dad235fc
thijew <thijewind@hotmail.com>
2022-06-21 16:39:55 +0200

    ADD Started the project

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+//Home page
+app.get('/', (req, res) => {
+    res.render('pages/index');
+  });
