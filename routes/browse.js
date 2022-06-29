100ec715aa092ee83d07546130ec8e3ed15631b8
thijew <thijewind@hotmail.com>
2022-06-29 23:02:28 +0200

    ik kan niet meer

--- routes/browse.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const browse = require('../controllers/browseMovies')
-
-router.get('/', browse.browse);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+// const multer = require('multer');
+const path = require('path');
+// const bodyParser = require('body-parser');
 
-require('dotenv').config();
+const dotenv = require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
@@ -12,55 +18,122 @@ app.listen(port, () => {
 app.set('view engine', 'ejs');
 app.set('views, view');
 
+app.use('/static', express.static(path.join(__dirname, 'static')))
+
+
+
+
+  ////////////////////////////////Mongo db////////////////////////////////
+  const { MongoClient } = require('mongodb');
+  const { ObjectId } = require("mongodb");
+
+const { error } = require('console');
+
+  let db;
+  let movies;
+
+  async function connectDB() {
+    const uri =  process.env.DB_URI;
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    try {
+      await client.connect();
+      db = client.db(process.env.DB_NAME);
+      movies = db.collection('movies');
+    } catch (error) {
+      throw error;
+    }
+  };
+  connectDB().then(console.log('we have a connection to mongo!'));
+  
+////////////  ///////////////////MULTER////////////////////////////////
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     cb(null, "./static/images/movies");
+//   },
+//   filename(_req, file, cb) {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+// const upload = multer({ storage });
+
 ////////////////////////////////ROUTES////////////////////////////////
 //Home page
-app.get('', (req, res) => {
-    res.render('pages/home');
-  });
-//Add page
-app.get('/add', (req, res) => {
-    res.render('pages/add');
+app.get('/', (req, res) => {
+  res.render('pages/home');
 });
+//Add page
+app.get('/add-movie', (req, res) => {
+  res.render('pages/add-movie');
+});
+// add movie to mongodb
+app.post('/add-movie', async (req, res) => {
+  console.log(req.body.title);
+  try {
+    const addMovie = await movies.insertOne({
+      title: "TEST",
+      director: req.body.director,
+      genre: req.body.genre,
+      rating: req.body.rating,
+      
+    });
+    console.log(addMovie);
+    const insertedId = addMovie.insertedId;
+    res.send('hij doet het');
+  } catch (err) {
+    res.render('pages/add-movie', {error: error.message });
+  }
+});
+
 // browse page
 app.get('/browse', (req, res) => {
-    res.render('pages/browse');
+  res.render('pages/browse');
 });
 
-const addMovie = require('./routes/add');
-app.use('/add', addMovie);
+//edit page
+app.get('/edit-movie', (req, res) => {
+    
+    const movie = db.collection('movies').findOne({},{});
+    res.render('pages/edit-movie', { movie });
+    
+});
 
 
-  ////////////////////////////////Mongo db////////////////////////////////
-const { MongoClient } = require('mongodb');
-// Connection URI
-const uri = "mongodb+srv://thijew:MMMovieMatch@blok-tech.2xkx5.mongodb.net/test";
-// const uri =
-//     'mongodb+srv://' +
-//     process.env.DB_USERNAME +
-//     ':' +
-//     process.env.DB_PASS +
-//     '@' +
-//     process.env.DB_HOST +
-//     '/' +
-//     process.env.DB_NAME +
-//     '/?retryWrites=true&w=majority';
-const client = new MongoClient(uri);
-// Database Name
-const dbName = 'MovieMatch';
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('movies');
-  return 'done.';
-}
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
-  ////////////////////////////////Mongo db////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   // 404
 app.use((req, res) => {
